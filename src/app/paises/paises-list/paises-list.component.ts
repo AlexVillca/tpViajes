1ae99fdda3d8eb815448fc73a55e4c6893d1c0d8
Facundo Ramallo <facu.ramallo@gmail.com>
2024-10-25 21:01:25 -0300

    agregado pipe filtro-paises, agregado filtro al html de lista

--- src/app/paises/paises-list/paises-list.component.ts
+++ src/app/paises/paises-list/paises-list.component.ts
@@ -6,6 +6,7 @@ import { PaisDataService } from '../pais-data.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-paises-list',
   standalone: true,
@@ -22,15 +23,35 @@ export class PaisesListComponent implements OnInit {
     private http: HttpClient
   ) { }
 
+  letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  letraSeleccionada = 'todos';
+  paisesFiltrados: Pais[] = [];
+
+  seleccionarLetra(letra: string) {
+    this.letraSeleccionada = letra;
+  }
+
+  filtrarPaises(letra: string) {
+    if (!letra || letra === 'todos') {
+      this.paisesFiltrados = this.paises;
+    } else {
+      this.paisesFiltrados = this.paises.filter(pais => pais.nombre.toLowerCase().startsWith(letra.toLowerCase()));
+    }
+  }
+
+
   ngOnInit(): void {
     this.http.get<Pais[]>('http://localhost:3000/paises') // Obtener datos de la API
       .subscribe(data => {
         this.paises = data;
       });
+      this.paisesFiltrados = this.paises;
   }
 
   seleccionarPais(pais: Pais) {
     this.paisDataService.setPais(pais);
     this.router.navigate(['/pais']);
   }
+
 }
+
