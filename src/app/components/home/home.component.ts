1838c8bb8eb119be06d8d567ae38edee11ccbb48
Alex Villca <villcaalex18@gmail.com>
2024-11-02 13:02:06 -0300

    creacion del componente game, incorporacion en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+
+import { GameComponent } from '../../game/game.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,GameComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
