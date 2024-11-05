f8a7c89c05df2cea23dea4385f414dee269d9567
manuel <manuelruggeri2@gmail.com>
2024-11-05 20:56:42 -0300

    se elimina componente duplicado

--- src/app/components/paises/ciudad-detail/ciudad-detail.component.ts
+++ src/app/components/paises/ciudad-detail/ciudad-detail.component.ts
@@ -3,17 +3,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CiudadDataService } from '../../../core/service/ciudad-data.service';
 import { Location } from '@angular/common';
+import { SelecionadorListasFlotanteComponent } from '../../favoritos/selecionador-listas-flotante/selecionador-listas-flotante.component';
+
 
 
 @Component({
   selector: 'app-ciudad-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,SelecionadorListasFlotanteComponent],
   templateUrl: './ciudad-detail.component.html',
   styleUrls: ['./ciudad-detail.component.css']
 })
 export class CiudadDetailComponent {
   ciudad$ = this.ciudadDataService.ciudad$;
+
+  showPopup = false;
 
   constructor(private ciudadDataService: CiudadDataService,  private location: Location) { }
 
