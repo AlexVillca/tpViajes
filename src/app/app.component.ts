e851184fa83bcbd0ddac94076ad08982d5b65f44
manuel <manuelruggeri2@gmail.com>
2024-11-08 20:55:34 -0300

    limpieza de imports innecesarios en el comp. principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,6 @@
 import { Component } from '@angular/core';
-
-import { LoginFormComponent } from './core/auth/login-form/login-form.component';
-import { RegisterComponent } from './core/auth/register/register.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from "./components/footer/footer.component";
-import { PaisesListComponent } from './components/paises/paises-list/paises-list.component';
-import { PaisDetailComponent } from './components/paises/pais-detail/pais-detail.component';
-
-import { FiltroPaisesPipe } from './pipes/filtro-paises.pipe';
-import { CiudadDetailComponent } from './components/paises/ciudad-detail/ciudad-detail.component';
-import { GameComponent } from './game/game.component';
 import { RouterOutlet } from '@angular/router';
 
 
@@ -19,21 +9,14 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [
     RouterOutlet,
-    LoginFormComponent,
-    RegisterComponent,
     NavbarComponent,
-    HttpClientModule,
-    FooterComponent,
-    FooterComponent,
-    PaisesListComponent,
-    PaisDetailComponent,
-    FiltroPaisesPipe,
-    GameComponent,
-    CiudadDetailComponent
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
 
 }
