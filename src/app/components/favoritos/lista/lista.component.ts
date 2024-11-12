c31a5f11abe76b4314783a1f4df0e0a2e2cfc0e2
manuel <manuelruggeri2@gmail.com>
2024-11-11 21:02:47 -0300

    arreglo bug en comp. lista de favoritos
    se arreglo bug que causaba la eliminacion de varias ciudades al eliminar una

--- src/app/components/favoritos/lista/lista.component.ts
+++ src/app/components/favoritos/lista/lista.component.ts
@@ -1,5 +1,5 @@
-import { CiudadEnLista } from './../../../models/interface/usuario.interface';
-import { Component, inject, OnInit } from '@angular/core';
+import { CiudadEnLista, ListaFav } from './../../../models/interface/usuario.interface';
+import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { UsuariosService } from '../../../core/service/usuarios.service';
 import { IdUsuarioService } from '../../../core/service/id-usuario.service';
@@ -7,16 +7,17 @@ import { CiudadDataService } from '../../../core/service/ciudad-data.service';
 import { Ciudad, Pais } from '../../../models/interface/pais.interface';
 import { PaisDataService } from '../../../core/service/pais-data.service';
 import { PaisesService } from '../../../core/service/paises.service';
-
+import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.css'
 })
-export class ListaComponent implements OnInit{
+export class ListaComponent implements OnInit,OnDestroy{
   ars = inject(ActivatedRoute);
   us = inject(UsuariosService);
   ids = inject(IdUsuarioService);
@@ -24,8 +25,14 @@ export class ListaComponent implements OnInit{
   paisesService = inject(PaisDataService);
   router = inject(Router);
   ps = inject(PaisesService);
-  lista:any; //listaFav
+  locationService = inject(Location);
+  lista:ListaFav = {
+    idLista:"",
+    nombreLista:"",
+    listaCiudades: []
+  }; //listaFav
   nombre:string ="";
+
   ngOnInit(): void {
     this.ars.paramMap.subscribe(
       {
@@ -38,7 +45,14 @@ export class ListaComponent implements OnInit{
                   this.us.obtenerListasFav(idUsuario).subscribe(
                     {
                       next:(listas)=>{
-                        this.lista = listas.find(l => l.idLista === idListaRecibido);
+
+                          this.lista = listas.find(l => l.idLista === idListaRecibido) ??
+                          {
+                            idLista: "",
+                            nombreLista: "",
+                            listaCiudades: []
+                          };
+
 
                       },
                       error:(error)=>{console.log(error)}
@@ -88,35 +102,55 @@ export class ListaComponent implements OnInit{
 
   }
   eliminarDeLaLista(ciudad:CiudadEnLista){
-      this.lista.listaCiudades = this.lista.listaCiudades.filter( (c:CiudadEnLista)=> c.nombre !== ciudad.nombre && c.codigoPais !== ciudad.codigoPais );
-      this.ids.id$.subscribe(
-        {
-          next:(id) => {
-            if(id){
-              this.us.getUsuarioById(id).subscribe(
-                {
-                  next:(u) => {
-                    if(u){
-                      const pos = u.listasFavs.findIndex(l => l.idLista === this.lista.idLista);
-                      u.listasFavs[pos] = this.lista;
-                      this.us.actualizarUsuario(u).subscribe(
-                        {
-                          next:() => {console.log("lista favs actualizada")},
-                          error:(e) => {console.log(e)}
-                        }
-                      );
-                    }
 
-                  },
-                  error:() => {console.log("ERROR:no se encontro al usuario")}
-                }
-              );
-            }
+    let index = this.lista.listaCiudades.indexOf(ciudad);
+    if(index >= 0){
+      this.lista.listaCiudades.splice(index,1);
+    }else{
+      console.log("error, no hay un elemento que cumpla el requisito");
+    }
+  }
+  ngOnDestroy(): void {
+    this.guardarCambios();
+  }
+  guardarCambios(){
+    this.ids.id$.subscribe(
+      {
+        next:(id) => {
+          if(id){
+            this.us.getUsuarioById(id).subscribe(
+              {
+                next:(u) => {
+                  if(u){
+                    const pos = u.listasFavs.findIndex(l => l.idLista === this.lista.idLista);
+                    u.listasFavs[pos] = this.lista;
+                    this.us.actualizarUsuario(u).subscribe(
+                      {
+                        next:() => {console.log("lista favs actualizada")},
+                        error:(e) => {console.log(e)}
+                      }
+                    );
+                  }
+
+                },
+                error:() => {console.log("ERROR:no se encontro al usuario")}
+              }
+            );
+          }
 
 
-          },
-          error:() => {console.log("ERROR:no se pudo obtener el id");}
-        }
-      );
+        },
+        error:() => {console.log("ERROR:no se pudo obtener el id");}
+      }
+    );
+  }
+  eliminarLista(){
+
+    this.locationService.back();
+
+  }
+
+  volver() {
+    this.locationService.back();
   }
 }
