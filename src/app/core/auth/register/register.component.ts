605a369828ab378ea5c4d7882e9842cf7e6ea222
manuel <manuelruggeri2@gmail.com>
2024-11-13 00:58:41 -0300

    renovacion de comp. register
    Se cambio la manipulacion del formulario a reactive form

--- src/app/core/auth/register/register.component.ts
+++ src/app/core/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { UsuariosService } from '../../service/usuarios.service';
@@ -9,14 +9,22 @@ import { Usuario } from '../../../models/interface/usuario.interface'
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule,CommonModule,RouterModule], // Asegúrate de importar los módulos necesarios aquí
+  imports: [ReactiveFormsModule,CommonModule,RouterModule], // Asegúrate de importar los módulos necesarios aquí
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  @ViewChild('registerForm') registerForm!: NgForm;
+
   routerService = inject(Router);
   usuarioService = inject(UsuariosService);
+  registroExitoso = false;
+  fb = inject(FormBuilder)
+  formulario = this.fb.nonNullable.group({
+    username:["",[Validators.required,Validators.minLength(4),Validators.maxLength(20)]],
+    email:["",[Validators.required,Validators.email]],
+    password:["",[Validators.required,Validators.minLength(8),Validators.maxLength(20)]],
+
+  });
   user:Usuario = {
     username: '',
     email: '',
@@ -27,73 +35,70 @@ export class RegisterComponent {
         idLista:Math.random().toString(36).substring(2, 9),
         nombreLista:"Guardadas",
         listaCiudades:[]
-      }
-    ]
+      }]
   };
+
   emailInvalid:Boolean = false;
   userNameInvalid:Boolean = false;
 
-  /*
-  this.usuarioService.postUsuario(this.user).subscribe({
-              next: (response) => {
-                console.log('Usuario creado exitosamente:', response);
-              },
-              error: (error) => {
-                console.error('Error al crear usuario:', error);
-              }
-            });
-  */
 
 
 
 
   onSubmit() {
+    this.emailInvalid = false;
+    this.userNameInvalid = false;
+    if (this.formulario.invalid) {return}
+    this.user.username = this.formulario.get('username')?.value || "";
+    this.user.email = this.formulario.get('email')?.value || "";
+    this.user.password = this.formulario.get('password')?.value || "";
+    this.usuarioService.comprobarEmailUsuario(this.user.email).subscribe(
+      {
+      next: (emailDisponible) => {
+        if (emailDisponible) {
+          console.log("email disponible");
+          this.emailInvalid = false;
+          this.usuarioService.comprobarUserNameUsuario(this.user.username).subscribe(
+            {
+              next: (usernameDisponible) => {
+                if(usernameDisponible){
+                  console.log("username aceptado");
+                  this.userNameInvalid = false;
 
-    if (this.registerForm.valid) {
+                  this.usuarioService.postUsuario({...this.user})
+                  .subscribe(
+                    {
+                    next: (response) => {
+                      console.log(response);
+                      this.registroExitoso = true;
 
-      this.usuarioService.comprobarEmailUsuario(this.user.email).subscribe(
-        {
-        next: (emailDisponible) => {
-          if (emailDisponible) {
-            this.emailInvalid = false;
-            this.usuarioService.comprobarUserNameUsuario(this.user.username).subscribe(
-              {
-                next: (usernameDisponible) => {
-                  if(usernameDisponible){
-                    console.log("username aceptado");
-                    this.userNameInvalid = false;
-
-                    this.usuarioService.postUsuario(this.user)
-                    .subscribe(
-                      {
-                      next: (response) => {
-                        console.log('Usuario creado exitosamente:', response);
+                      setTimeout(() => {
                         this.routerService.navigate(['login']);
-                       },
-                      error: (error) => { console.error('Error al crear usuario:', error); }
-                      }
-                    );
+                      }, 3000);
+                     },
+                    error: (error) => { console.error('Error al crear usuario:', error); }
+                    }
+                  );
 
-                  }else{
-                    console.error('El username esta en uso');
-                    this.userNameInvalid = true;
-                  }
-                },
-              error: (error) => {
-                console.error('Error al comprobar el username:', error);
-              }
-              }
-            );
-          } else {
-            console.log('El email ya está en uso.');
-            this.emailInvalid = true;
+                }else{
+                  console.error('El username esta en uso');
+                  this.userNameInvalid = true;
+                }
+              },
+            error: (error) => {
+              console.error('Error al comprobar el username:', error);
+            }
+            }
+          );
+        } else {
+          console.log('El email ya está en uso.');
+          this.emailInvalid = true;
 
-          }
-        },
-        error: (error) => { console.error('Error al comprobar el email:', error); }
         }
-      );
-    }
+      },
+      error: (error) => { console.error('Error al comprobar el email:', error); }
+      }
+    );
   }
 
 
