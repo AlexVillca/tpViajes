6f84c4005a642b0341c2c15120fad0c8bf30a1da
Facundo Ramallo Sanjuan <facu.ramallo@gmail.com>
2024-11-16 11:51:05 -0300

    update por patch con observable en service arreglado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,17 @@ import { HomeComponent } from './components/home/home.component';
 import { PaisesListComponent } from './components/paises/paises-list/paises-list.component';
 import { PaisDetailComponent } from './components/paises/pais-detail/pais-detail.component';
 import { CiudadDetailComponent } from './components/paises/ciudad-detail/ciudad-detail.component';
-import { Component } from '@angular/core';
 import { GameComponent } from './game/game.component';
 import { Routes } from '@angular/router';
 import { ListaDeListasComponent } from './components/favoritos/lista-de-listas/lista-de-listas.component';
 import { ListaComponent } from './components/favoritos/lista/lista.component';
 import { ContactUsComponent } from './components/home/contact-us/contact-us.component';
 import { AboutUsComponent } from './components/home/about-us/about-us.component';
+import { UpdatePasswordComponent } from './core/auth/update-password/update-password.component';
 
 
 export const routes: Routes = [
+
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginFormComponent },
@@ -26,6 +27,8 @@ export const routes: Routes = [
   { path: 'favoritos', component: ListaDeListasComponent},
   { path: 'favoritos/listaCiudadesComp/:idLista', component:ListaComponent},
   { path: 'contact', component:ContactUsComponent},
-  { path: 'about', component:AboutUsComponent}
+  { path: 'about', component:AboutUsComponent},
+  { path:'updateP', component:UpdatePasswordComponent},
+  { path:'**',redirectTo: ''}
 
 ];
