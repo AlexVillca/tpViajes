a9e8de0521b56af3036118e1d1008287be9b5783
Facundo Ramallo Sanjuan <facu.ramallo@gmail.com>
2024-11-16 12:36:12 -0300

    se agrega ficha user

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
 import { IdUsuarioService } from '../../core/service/id-usuario.service';
 import { CommonModule } from '@angular/common';
+import { UsuariosService } from '../../core/service/usuarios.service';
 
 
 
@@ -13,24 +14,38 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
   idUsuarioService = inject(IdUsuarioService);
+  usuariosService = inject(UsuariosService);
   routerService = inject(Router);
   flag:boolean = false;
-  ngOnInit() {
-    this.idUsuarioService.id$.subscribe((valor) => {
+  username: string | null = null;
 
-      if(valor !== null){
+  ngOnInit() {
+    this.idUsuarioService.id$.subscribe((id) => {
+      if (id !== null) {
         this.flag = true;
 
-
-      }else{
+        this.usuariosService.getUsuarioById(id).subscribe({
+          next: (usuario) => {
+            this.username = usuario.username;
+          },
+          error: (e) => {
+            console.error('Error al obtener el usuario:', e);
+            this.username = null; // Resetea el nombre si hay un error
+          },
+        });
+      } else {
         this.flag = false;
+        this.username = null; // Resetea el nombre si no hay usuario logueado
       }
     });
   }
+
   logOut(){
     this.idUsuarioService.clearUserId();
     this.routerService.navigate(['home']);
   }
+
 }
