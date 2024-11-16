e456774a6d6cd3b565972cfa016a8701f8545cf4
Facundo Ramallo Sanjuan <facu.ramallo@gmail.com>
2024-11-16 12:48:18 -0300

    se agrega ficha user

--- src/app/models/interface/usuario.interface.ts
+++ src/app/models/interface/usuario.interface.ts
@@ -2,7 +2,7 @@ export interface Usuario{
     id?:string,
     username: string,
     email: string,
-    password: string
+    password: string,
     listasFavs: ListaFav[]
 }
 export interface ListaFav{
