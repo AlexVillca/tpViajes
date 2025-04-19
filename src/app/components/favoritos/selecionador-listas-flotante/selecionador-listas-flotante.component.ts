ab86f115fc741ca10e87b84a8d76f09d83df84b6
manuel <manuelruggeri2@gmail.com>
2025-04-19 20:43:13 -0300

    refactorización de comp. seleccionador de paises
    se dividieron responsabilidades en funciones y se agregaron comentarios para hacer el código mas legible

--- src/app/components/favoritos/selecionador-listas-flotante/selecionador-listas-flotante.component.ts
+++ src/app/components/favoritos/selecionador-listas-flotante/selecionador-listas-flotante.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, inject, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { ListaFav } from '../../../models/interface/usuario.interface';
 import { CommonModule } from '@angular/common';
-import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsuariosService } from '../../../core/service/usuarios.service';
 import { IdUsuarioService } from '../../../core/service/id-usuario.service';
 import { PaisDataService } from '../../../core/service/pais-data.service';
 import { CiudadDataService } from '../../../core/service/ciudad-data.service';
-import { RegisterComponent } from '../../../core/auth/register/register.component';
+
 interface ListaCheckbox {
   id:string;
   nombre: string;
@@ -21,7 +21,7 @@ interface ListaCheckbox {
   templateUrl: './selecionador-listas-flotante.component.html',
   styleUrl: './selecionador-listas-flotante.component.css'
 })
-export class SelecionadorListasFlotanteComponent implements OnInit{
+export class SelecionadorListasFlotanteComponent implements OnInit,OnDestroy{
 
   fb = inject(FormBuilder);
   us = inject(UsuariosService);
@@ -31,94 +31,107 @@ export class SelecionadorListasFlotanteComponent implements OnInit{
   visible = false;
   logueado = false;
 
-  listaDeListasDB:ListaFav[] = [];
+  listasFavoritosDB:ListaFav[] = [];
   listasFront:ListaCheckbox[] = [];
-
-
-
 
   ciudadSeleccionada:any;
   paisSeleccionado:any;
 
   alertaMax = false;
 
-  formulario:any;
+  formulario:FormGroup = this.fb.group({
+    nuevocheckboxListaFavorito: ['', Validators.required],
+    checkboxesListasFavoritos: this.fb.array([])
 
-  constructor(){
+  });
 
-
-
-  }
-
-  createItemControl(item: any): FormGroup {
+  //
+  crearCheckboxForm(item: any): FormGroup {
     return this.fb.group({
       id: [item.id],
       nombre: [item.nombre],
       seleccionada: [item.seleccionada]
     });
   }
-
-  get itemsArray(): FormArray {
-    return this.formulario.get('items') as FormArray;
+  //con esto trabajo directamente en los checkbox
+  get arrayCheckbox(): FormArray {
+    return this.formulario.get('checkboxesListasFavoritos') as FormArray;
   }
+  //y con el input para una nueva lista
+  get nuevaListaInput():FormControl {
+    return this.formulario.get('nuevocheckboxListaFavorito') as FormControl;
+  }
+
+
+
   ngOnInit(): void {
     this.ids.id$.subscribe(
       {
         next: (id) => {
           if(id){
             this.logueado = true;
+
             this.us.obtenerListasFav(id).subscribe(
                 {
                   next:(l) => {
-                    this.listaDeListasDB = l
-                    this.listasFront = this.listaDeListasDB.map(lista => this.mapDBaFront(lista));
-                    this.formulario = this.fb.group({
-                      nuevoItem: ['', Validators.required],
-                      items: this.fb.array(this.listasFront.map(item => this.createItemControl(item)))
-                      //
-                    });
-
-
+                    this.listasFavoritosDB = l;
+                    //obtengo las listas
+                    this.pasajeDBaFront();
+                    //cargo las checkbox
+                    this.pasajeListaFrontAFormulario();
                   },
                   error: (e) => {console.log(e)}
                 }
             )
+            this.paisDataService.pais$.subscribe(
+              {
+                next:(p) => {this.paisSeleccionado = p},
+                error:(error) => {console.log(error)}
+              }
+            );
+            this.ciudadDataService.ciudad$.subscribe(
+              {
+                next:(c) => {this.ciudadSeleccionada = c},
+                error:(error) => {console.log(error)}
+              }
+            );
           }
         },
         error: (e) => {console.log(e)}
       }
     );
 
-    this.paisDataService.pais$.subscribe(
-      {
-        next:(p) => {this.paisSeleccionado = p},
-        error:(error) => {console.log(error)}
-      }
-    );
-    this.ciudadDataService.ciudad$.subscribe(
-      {
-        next:(c) => {this.ciudadSeleccionada = c},
-        error:(error) => {console.log(error)}
-      }
-    );
+
   }
+
+  //generar un id unico
+  private nuevoIdLista():string{
+    let idGenerado:string;
+    do{
+      idGenerado = Math.random().toString(36).substring(2, 9);
+    }while(this.listasFront.some( l => l.id === idGenerado));
+    return idGenerado;
+  }
+
+
 
  // Agrega un nuevo item al FormArray
  agregarNuevaLista() {
 
-  if(this.itemsArray.length < 6){
+  if(this.arrayCheckbox.length < 6){
     console.log("entro cant listas");
     console.log(this.formulario.value);
 
-    if (this.formulario.get('nuevoItem')?.valid) {
+    if (this.nuevaListaInput.valid) {
       console.log("entro form valido");
-    const nuevoItem = {
-      id: Math.random().toString(36).substring(2, 9),
-      nombre: this.formulario.get('nuevoItem')?.value,
-      seleccionada: true
-    };
-    this.itemsArray.push(this.createItemControl(nuevoItem));
-    this.formulario.get('nuevoItem')?.reset();
+
+      const nuevoItem = {
+        id:this.nuevoIdLista() ,
+        nombre: this.nuevaListaInput.value,
+        seleccionada: true
+      };
+      this.arrayCheckbox.push(this.crearCheckboxForm(nuevoItem));
+      this.nuevaListaInput.reset();
     }else{
 
     }
@@ -130,46 +143,44 @@ export class SelecionadorListasFlotanteComponent implements OnInit{
     }, 4000);
   }
 }
-
-
-
-
-  private mapDBaFront(lista: ListaFav):ListaCheckbox{
-    let selec;
-    if(lista.listaCiudades.some(c => c.codigoPais === this.paisSeleccionado?.codigo && c.nombre === this.ciudadSeleccionada?.nombre)){
+  private pasajeDBaFront(){
+    this.listasFront = this.listasFavoritosDB.map(lista => {
+      let selec;
+      if(lista.listaCiudades.some(c => c.codigoPais === this.paisSeleccionado?.codigo && c.nombre === this.ciudadSeleccionada?.nombre)){
         selec = true;
-    }else{
-      selec = false;
-    }
-    return {
-      id: lista.idLista,
-      nombre: lista.nombreLista,
-      seleccionada: selec
-    };
+      }else{
+        selec = false;
+      }
+      return {
+        id: lista.idLista,
+        nombre: lista.nombreLista,
+        seleccionada: selec
+      };
+    });
   }
-
-
-  private pasajeCambiosFrontaDB(){
+  private pasajeFrontaDB(){
     this.listasFront.forEach(l => {
-      let listaOriginal = this.listaDeListasDB.find(ldb => ldb.idLista === l.id);
-      if(listaOriginal === undefined){ // se es una lista nueva
+      let listaOriginal = this.listasFavoritosDB.find(ldb => ldb.idLista === l.id);
+      if(listaOriginal === undefined){ // si es una lista nueva
         listaOriginal = { //la agrega
           idLista:l.id,
           nombreLista:l.nombre,
           listaCiudades:[]
         }
-        this.listaDeListasDB.push(listaOriginal);
+        this.listasFavoritosDB.push(listaOriginal);
       }
-      //veo si concuerda
-      //si posee la ciudad en la lista y
+      //veo si
+      //la ciudad esta en la lista original y en la front o si no esta en la original y no esta en la front
       if(listaOriginal.listaCiudades.some(cl => cl.codigoPais === this.paisSeleccionado.codigo && cl.nombre === this.ciudadSeleccionada.nombre) && l.seleccionada ||
         !listaOriginal.listaCiudades.some(cl => cl.codigoPais === this.paisSeleccionado.codigo && cl.nombre === this.ciudadSeleccionada.nombre) && !l.seleccionada){
-          ///sin cambios
+          ///la lista se mantiene sin cambios
           console.log("la lista se mantiene");
       }else if(l.seleccionada){
+        //se agrega la ciudad
         console.log("agrega");
           listaOriginal.listaCiudades.push({codigoPais:this.paisSeleccionado.codigo,nombre:this.ciudadSeleccionada.nombre});
       }else{
+        //se quita la ciudad
           console.log("saca");
           listaOriginal.listaCiudades = listaOriginal.listaCiudades.filter(elementoFav => !(elementoFav.codigoPais === this.paisSeleccionado.codigo && elementoFav.nombre === this.ciudadSeleccionada.nombre));
 
@@ -177,15 +188,29 @@ export class SelecionadorListasFlotanteComponent implements OnInit{
     });
 
   }
- pasarFormaListaFront() {
+  pasarFormularioAListaFront() {
+    this.listasFront = this.arrayCheckbox.value;
 
-
-    this.listasFront = this.formulario.value.items;
-    console.log(this.listasFront);
+  }
+  pasajeListaFrontAFormulario(){
+    this.formulario.setControl('checkboxesListasFavoritos',this.fb.array(this.listasFront.map(l => this.crearCheckboxForm(l))))
   }
   saveSelection() {
-    this.pasarFormaListaFront();
-    this.pasajeCambiosFrontaDB();
+    this.pasarFormularioAListaFront();
+    this.pasajeFrontaDB();
+
+
+    this.visible = false;
+  }
+
+  closePopup() {
+
+    this.nuevaListaInput.reset();
+    this.pasajeListaFrontAFormulario();
+    console.log("se cierra");
+    this.visible = false;
+  }
+  ngOnDestroy(){
     this.ids.id$.subscribe(
       {
         next:(id) => {
@@ -194,7 +219,7 @@ export class SelecionadorListasFlotanteComponent implements OnInit{
               {
                 next:(u) => {
                   if(u){
-                    u.listasFavs = this.listaDeListasDB;
+                    u.listasFavs = this.listasFavoritosDB;
                     this.us.actualizarUsuario(u).subscribe(
                       {
                         next:() => {console.log("lista favs actualizada")},
@@ -214,18 +239,6 @@ export class SelecionadorListasFlotanteComponent implements OnInit{
         error:() => {console.log("ERROR:no se pudo obtener el id");}
       }
     );
-
-    this.visible = false;
-  }
-
-  closePopup() {
-    this.formulario = this.fb.group({
-      nuevoItem: ['', Validators.required],
-      items: this.fb.array(this.listasFront.map(item => this.createItemControl(item)))
-
-    });
-    console.log("se cierra");
-    this.visible = false;
   }
 
 }
