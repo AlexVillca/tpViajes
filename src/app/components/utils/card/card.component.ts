b4bd9b12e9c857ab46dc0db34d1bf7b1bad11108
manuel <manuelruggeri2@gmail.com>
2025-12-09 23:47:55 -0300

    refactor frame a card
    Se recreo el componente "frame" manteniendo la lógica, pero reorganizando las etiquetas que lo forman y dando los estilos desde cero, también se ha renombrado a "card" un nombre mas comun para el componente.

--- src/app/components/utils/card/card.component.ts
+++ src/app/components/utils/card/card.component.ts
@@ -1,30 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-frame',
+  selector: 'app-card',
   standalone: true,
   imports: [],
-  templateUrl: './frame.component.html',
-  styleUrl: './frame.component.css'
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
 })
-export class FrameComponent {
- @Input() titulo:string = "";
- @Input() arregloImagenes:string[] = [];
- @Input() idLista:string = ""
-
+export class CardComponent {
+  @Input() titulo:string = "";
+  @Input() arregloImagenes:string[] = [];
+  @Input() idLista:string = ""
 
   imagenActual: string = '';
   private index = 0;
   private intervalo: any;
   girando = false;
+
   ngOnInit(): void {
     this.imagenActual = this.arregloImagenes[0];
   }
+
   iniciarCarrusel() {
+    if(this.arregloImagenes.length == 1){
+      return
+    }
     this.girando = true;
     this.imagenActual = this.arregloImagenes[this.index];
-
     this.intervalo = setInterval(() => {
       this.index = (this.index + 1) % this.arregloImagenes.length;
       this.imagenActual = this.arregloImagenes[this.index];
