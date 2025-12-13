2ad7a2274e7027a49e71a3a005e8700fe6fff903
manuel <manuelruggeri2@gmail.com>
2025-12-13 18:33:50 -0300

    refactorización de lista de listas
    Se refactorizo haciendo al comp. ldls. responsive, además se uso el comp. card para mejorar la presentacion visual de las listas.

--- src/app/components/favoritos/lista-de-listas/lista-de-listas.component.ts
+++ src/app/components/favoritos/lista-de-listas/lista-de-listas.component.ts
@@ -2,69 +2,99 @@ import { Component, inject, OnInit } from '@angular/core';
 import { UsuariosService } from '../../../core/service/usuarios.service';
 import { ListaFav } from '../../../models/interface/usuario.interface';
 import { IdUsuarioService } from '../../../core/service/id-usuario.service';
-import { Router, RouterLink, RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Location } from '@angular/common';
+import { ImagenesLista } from '../../../models/interface/imagenesLista.interface';
+import { PaisesService } from '../../../core/service/paises.service';
+import { Pais } from '../../../models/interface/pais.interface';
+import { CardComponent } from "../../utils/card/card.component";
 
 @Component({
   selector: 'app-lista-de-listas',
   standalone: true,
-  imports: [RouterModule,RouterLink],
+  imports: [RouterModule, /*RouterLink,*/ CardComponent],
   templateUrl: './lista-de-listas.component.html',
   styleUrl: './lista-de-listas.component.css'
 })
-export class ListaDeListasComponent implements OnInit{
-    us = inject(UsuariosService);
-    ids = inject(IdUsuarioService);
-    router = inject(Router);
+export class ListaDeListasComponent implements OnInit {
+  us = inject(UsuariosService);
+  ids = inject(IdUsuarioService);
+  router = inject(Router);
+  paisesService = inject(PaisesService);
+  locationService = inject(Location);
 
-    listaDeListas:ListaFav[] = [];
+  listas: ImagenesLista[] = [];
 
-    containerVisible = false;
 
-    ngOnInit(): void {
-      this.ids.id$.subscribe({
-        next:(idObtenido) => {
-            if(idObtenido){
-              this.us.obtenerListasFav(idObtenido).subscribe(
-                {
-                  next:(lista)=> {this.listaDeListas = lista},
-                  error:(e)=> {console.log(e)}
-                }
-              )
-            }else{
-              console.log("error: no se a podido obtener el id");
-            }
+  ngOnInit(): void {
+    this.ids.id$.subscribe(
+      {
+        next: (idObtenido) => {
+          if (idObtenido) {
+            this.us.obtenerListasFav(idObtenido).subscribe(
+              {
+                next: (lista) => {
+                  this.paisesService.getPaises().subscribe({
+                    next: (paises) => {
+                      this.obtenerImagenesListas(lista, paises);
+                    },
+                    error: (e) => { console.log(e) }
+                  });
+                },
+                error: (e) => { console.log(e) }
+              }
+            );
+          } else {
+            console.log("error: no se ah encontrado el id");
+          }
         },
-        error:(e) => {console.log(e);}
+        error: (e) => { console.log(e); }
       });
+  }
 
-      setTimeout(() => {
-        this.containerVisible = true;
-      }, 200); // Retraso en milisegundos
-    }
-    accederLista(lista:any){
-        this.router.navigate(['listaCiudades']);
-    }
+  accederLista(idLista: string) {
+    this.router.navigate(['/listaCiudadesComp',idLista]);
+  }
 
-
-    locationService = inject(Location);
   volver() {
     this.locationService.back();
   }
 
-  videoLoaded = false;
+  obtenerImagenesListas(listasFav: ListaFav[], paises: Pais[]): void {
+    this.listas = listasFav.map(lista => {
+      const imagenes: string[] = [];
 
-  // Esta función se llama cuando el video se carga completamente
-  onVideoLoaded() {
-    this.videoLoaded = true;
-    setTimeout(() => {
-      const buttons = document.querySelectorAll('button');
+      lista.listaCiudades.forEach(ciudadEnLista => {
 
-      buttons.forEach((button) => {
-        button.classList.add('visible');  // Añadir la clase 'visible' a los botones
+        const pais = paises.find(p => p.codigo === ciudadEnLista.codigoPais);
+        if (!pais) return;
+
+        const ciudad = pais.ciudades?.find(c => c.nombre === ciudadEnLista.nombre);
+        if (!ciudad) return;
+
+
+        const imagenAtr = ciudad.atracciones?.find(a => a.imagen)?.imagen;
+        if (imagenAtr) { imagenes.push(imagenAtr); }
       });
-    }, 200);  // Se espera 200ms para asegurarse de que todo se renderice correctamente
+
+      return {
+        id: lista.idLista,
+        nombre: lista.nombreLista,
+        imagenes
+      };
+    });
+
   }
 
-  
+  containerVisible = false;
+  videoLoaded = false;
+
+  onVideoLoaded() {
+    this.videoLoaded = true;
+
+    setTimeout(() => {
+      this.containerVisible = true;
+    }, 200); // Retraso en milisegundos
+  }
+
 }
