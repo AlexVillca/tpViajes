ef0cf6cd52419a641bc510cb002c3d968a35983e
manuel <manuelruggeri2@gmail.com>
2026-03-09 19:11:15 -0300

    se agrega interfaz faltante para comp. lista
    Se agrega interfaz "ImagenesCiudad".

--- src/app/models/interface/imagenesLista.interface.ts
+++ src/app/models/interface/imagenesLista.interface.ts
@@ -1,5 +1,12 @@
+import { CiudadEnLista } from "./usuario.interface"
+
 export interface ImagenesLista{
   id:string,
   nombre:string,
+  imagenes:string[]
+}
+
+export interface ImagenesCiudad{
+  ciudad:CiudadEnLista
   imagenes:string[]
 }
